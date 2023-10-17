b9f27dbd2b47fbcba9cec97110899f5ec6597952
camilaescobar7 <27078136-camilaescobar7@users.noreply.replit.com>
2023-10-17 18:59:18 +0000

    teste

---
+++ AF2/main.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Análise de dados sobre focos de incêndio no cerrado ocorridos entre 1998 e 2023
+
+df_arquivo= pd.read_csv("arquivo.csv", index_col=0)
+#print (df_arquivo)
+
+#Somente os anos com todos os meses monitorados
+df_arquivo = df_arquivo.dropna()
+
+
+#Qual o total de focos de incêndio a cada ano?
+df_arquivo['Total anual'] = df_arquivo.sum(axis=1)
+
+#Qual os meses com maior e menor ocorrência de focos de incêndio em cada ano?
+df_arquivo['Mês intenso'] = df_arquivo.iloc[:, :-2].idxmax(axis=1)
+df_arquivo['Mês ameno'] = df_arquivo.iloc[:, :-2].idxmin(axis=1)
+
+#Substituindo um cabeçalho subjetivo por objetivo
+df_arquivo=df_arquivo.rename(columns={'Mês ameno': 'Menor ocorrência', 'Mês intenso': 'Maior ocorrência'})
+
+#Qual o total de focos de incêndio de cada mês ao longo dos anos?
+df_arquivo = df_arquivo.T
+df_arquivo['Total por mês'] = df_arquivo.iloc[:-3, :].sum(axis=1)
+df_arquivo = df_arquivo.T
+print(df_arquivo)
+
+#Quais os anos e o mês destes que setembro não registrou a maior ocorrência de focos?
+anos_distoantes = df_arquivo.iloc[:-1,-2]
+filter= anos_distoantes != "Setembro"
+anos_distoantes=anos_distoantes[filter]
+anos_distoantes=anos_distoantes.to_string()
+print(anos_distoantes)
+
+df_arquivo.to_csv("arquivo_final.csv", index=False)
+
